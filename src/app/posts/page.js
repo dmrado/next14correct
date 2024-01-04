4cfa674ad90c1b362a3db0eccf99714aa11b69c6
dmrado <dmrado@live.ru>
2024-01-04 09:51:39 +0300

    posts

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,10 +1,16 @@
 import Link from "next/link"
+const {sequelize} = require('../models')
+const Post = sequelize.models.Post
+import {revalidatePath} from "next/cache";
 
 
-const Posts = () => {
+const Posts = async () => {
+    const posts = await Post.findAll().then(res=> res.map(r => r.dataValues))
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             Posts
+            {posts}
         </main>
     )
 }
