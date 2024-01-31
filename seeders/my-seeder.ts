5652933e52fa3f437120f344d6cdb52a1781c9e8
Vladimir Kromm <vladimir.kromm@mercedes-benz.com>
2024-01-31 11:37:51 +0100

    fixed seeder from Dmitriy

--- seeders/my-seeder.ts
+++ seeders/my-seeder.ts
@@ -1,27 +1,28 @@
 import {Post} from "@/app/db/post.model";
 const posts = [
     {
-        id: 1,
+        // id: 1,
         title: 'Title Post-1111',
         text: 'Text Post-111, Text Post-1, Text Post-1,Text Post-1',
         createdAt: new Date(),
         updatedAt: new Date(),
     },
     {
-        id: 2,
+        // id: 2,
         title: 'Title Post-2',
         text: 'Text Post-2, Text Post-2, Text Post-2,Text Post-2, Text Post-2,Text Post-2',
         createdAt: new Date(),
         updatedAt: new Date(),
     },
     {
-        id: 3,
+        // id: 3,
         title: 'Title Post-3',
+        text: 'Text!!!!',
         createdAt: new Date(),
         updatedAt: new Date(),
     },
     {
-        id: 4,
+        // id: 4,
         title: 'Title Post-4',
         text: 'llam et saepe reiciendis voluptatem adipisciunt voluptatem rerum illo velits',
         createdAt: new Date(),
@@ -33,7 +34,7 @@ const posts = [
         createdAt: new Date(),
         updatedAt: new Date(),
     },
-],
+]
 
 // const seedPost = async ()=>{
 //     await Post.create(posts.map(post => ({title: post.title, text: post.text})))
