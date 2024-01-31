8746e9b0a1a1a28e5273e6765f47d38b2be7eac0
dmrado <dmrado@live.ru>
2024-01-31 13:15:58 +0300

    seeders need fix

--- seeders/my-seeder.ts
+++ seeders/my-seeder.ts
@@ -1,8 +1,46 @@
 import {Post} from "@/app/db/post.model";
+const posts = [
+    {
+        id: 1,
+        title: 'Title Post-1111',
+        text: 'Text Post-111, Text Post-1, Text Post-1,Text Post-1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        id: 2,
+        title: 'Title Post-2',
+        text: 'Text Post-2, Text Post-2, Text Post-2,Text Post-2, Text Post-2,Text Post-2',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        id: 3,
+        title: 'Title Post-3',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        id: 4,
+        title: 'Title Post-4',
+        text: 'llam et saepe reiciendis voluptatem adipisciunt voluptatem rerum illo velits',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        title: 'Title Post-5',
+        text: 'llam et saepe reiciendis voluptatem adipiscsunt voluptatem rerum illo velits',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+],
+
+// const seedPost = async ()=>{
+//     await Post.create(posts.map(post => ({title: post.title, text: post.text})))
+// }
 
 const seedPost = async ()=>{
-    await Post.create({title: 'New Post', text:"Post from seeder"})
+    await Post.bulkCreate(posts)
 }
 
 await seedPost()
-
--- seeders/unseed.ts
+++
@@ -1,4 +0,0 @@
-import { down } from "./post.seeder";
-import { sequelize } from "@/app/db/connection.ts";
-
-down(sequelize.getQueryInterface());
