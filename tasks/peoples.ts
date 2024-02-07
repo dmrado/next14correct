19388e616875dc9026948f5784ac644bd0ebe945
dmrado <dmrado@live.ru>
2024-02-07 21:08:29 +0300

    npx tsx

--- tasks/peoples.ts
+++ tasks/peoples.ts
@@ -1,4 +1,4 @@
-import {faker} from '@faker-js/faker';
+import { faker } from '@faker-js/faker'
 
 interface Person {
     name: string;
@@ -11,48 +11,47 @@ interface CategorizedPeople {
     senior: Person[];
 }
 
-const now = new Date();
+const now = new Date()
 
 // Функция для генерации возраста
 function generateAge(): number {
-    const birthDate = faker.date.birthdate({min: 0, max: 100, mode: "age"});
-    const age = now.getFullYear() - birthDate.getFullYear();
-    const monthDiff = now.getMonth() - birthDate.getMonth();
-    if (monthDiff < 0 || (monthDiff === 0 && now.getDate() < birthDate.getDate())) {
-        return age - 1;
-    }
-    return age;
-}
-
-// Функция для генерации полного имени
-function generateName(): string {
-    return faker.person.fullName();
+  const birthDate = faker.date.birthdate({ min: 0, max: 100, mode: 'age' })
+  const age = now.getFullYear() - birthDate.getFullYear()
+  if (age > 100 || age === 0) {
+    console.log(age)
+  }
+  const monthDiff = now.getMonth() - birthDate.getMonth()
+  if (monthDiff < 0 || (monthDiff === 0 && now.getDate() < birthDate.getDate())) {
+    return age - 1
+  }
+  return age
 }
 
 // Функция для генерации массива людей
 function generatePeople(count: number): Person[] {
-    const people: Person[] = [];
-    for (let i = 0; i < count; i++) {
-        people.push({
-            name: generateName(),
-            age: generateAge(),
-        });
-    }
-    return people;
+  const people: Person[] = []
+  for (let i = 0; i < count; i++) {
+    people.push({
+      name: faker.person.fullName(),
+      age: generateAge(),
+    })
+  }
+  return people
 }
 
-const people = generatePeople(100);
+const people = generatePeople(100)
 
-const categorizedPeoples = people.reduce<CategorizedPeople>((accum, currentValue) => {
-    if (currentValue.age < 20) {
-        accum.young.push(currentValue);
-    } else if (currentValue.age >= 20 && currentValue.age < 65) {
-        accum.workingAge.push(currentValue);
-    } else if (currentValue.age >= 65) {
-        accum.senior.push(currentValue);
-    }
-    return accum;
-}, {young: [], workingAge: [], senior: []});
+const initialValue = { young: [], workingAge: [], senior: [] } as CategorizedPeople
 
-console.log('Categorized Peoples:', categorizedPeoples);
+const categorizedPeoples = people.reduce((accum, currentValue) => {
+  if (currentValue.age < 20) {
+    accum.young.push(currentValue)
+  } else if (currentValue.age >= 20 && currentValue.age < 65) {
+    accum.workingAge.push(currentValue)
+  } else if (currentValue.age >= 65) {
+    accum.senior.push(currentValue)
+  }
+  return accum
+}, initialValue)
 
+console.log('Categorized Peoples:', categorizedPeoples)
