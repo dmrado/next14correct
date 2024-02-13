7c59907fa7d38ad8d40d71ae6078c5b212f2049c
dmrado <dmrado@live.ru>
2024-02-13 18:23:40 +0300

    need fix Header

--- src/app/api/auth/[...nextauth].js
+++ src/app/api/auth/[...nextauth].js
@@ -1,16 +1,14 @@
-import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-
 export const authOptions = {
-  // Конфигурация провайдеров
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  //это значение по умолчанию указано здесь для ясности
-  session: {
-    strategy: 'jwt',
-  },
+   // Конфигурация провайдеров
+   providers: [
+      GoogleProvider({
+         clientId: process.env.GOOGLE_CLIENT_ID,
+         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+   ],
+   //это значение по умолчанию указано здесь для ясности
+   session: {
+      strategy: 'jwt',
+   },
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,49 @@
 import { useSession, signIn, signOut, getSession } from 'next-auth/react'
-import RootLayout from '@/app/layout.js/RootLayout'
+import RootLayout from '@/app/layout'
 
 const clientId = process.env.clientId
 
 export async function getServerSideProps(ctx) {
-  const session = await getSession(ctx)
-  return {
-    props: { session }
-  }
+   const session = await getSession(ctx)
+   console.log('ctx', ctx)
+   console.log('session', ctx)
+   return {
+      props: { session }
+   }
 }
 
 const Header = () => {
-  // Содержит информацию об аутентифицированном пользователе
-  const { data: session, status } = useSession()
+   // Содержит информацию об аутентифицированном пользователе
+   const { data: session, status } = useSession()
 
-  // Статус пользователя
-  if (status === 'loading') return <h1>Загрузка...</h1>
-  if (status === 'authenticated') {
-    return (
+   // Статус пользователя
+   if (status === 'loading') return <h1>Загрузка...</h1>
+   if (status === 'authenticated') {
+      return (
+         <div>
+            <h1>Привет, {session.user.name}</h1>
+            <img src={session.user.image} alt='photo' />
+            <button className="hover:bg-[#D50000] p-2 rounded"
+               onClick={signOut}>Выйти</button>
+         </div>
+      )
+   }
+
+   return (
       <div>
-        <h1>Привет, {session.user.name}</h1>
-        <img src={session.user.image} alt='photo' />
-        <button onClick={signOut}>Выйти</button>
-      </div>
-    )
-  }
 
-  return (
-    <div>
-      <button onClick={() => signIn('google')}>Войти</button>
-    </div>
-  )
+         <button className="hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => signIn('google')}>Войти</button>
+      </div>
+   )
 }
 
 export default function AuthPage({ session }) {
-  return (
-    <RootLayout>
-      <Header session={session} />
-    </RootLayout>
-  )
+   return (
+      <RootLayout>
+         <Header session={session} />
+      </RootLayout>
+   )
 }
 
 // useEffect(() => {
@@ -68,17 +73,6 @@ export default function AuthPage({ session }) {
 // let accessToken = gapi.auth.getToken().access_token
 
 // return <div className="flex flex-wrap h-20 justify-center items-center">
-//   <a className="mr-3"
-//     href="/api/auth/signin">
-//     <button
-//       className="hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Войти с Google
-//     </button>
-//   </a>
-//
-//   <a href="/api/auth/signout">
-//     <button
-//       className="hover:bg-[#D50000] p-2 rounded">Выйти с Google
-//     </button>
-//   </a>
-//
+//   <a className="mr-3" href="/api/auth/signin"></s>
+//   <a href="/api/auth/signout"></a>
 // </div>
