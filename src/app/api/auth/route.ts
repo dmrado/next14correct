5aa34f08b9c450cecfd41c0e98612337f5a3ee7a
dmrado <dmrado@live.ru>
2024-02-17 12:02:03 +0300

    оформление

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -4,3 +4,6 @@
 // const handler = NextAuth(authOptions)
 
 // export { handler as GET, handler as POST }
+
+import { handlers } from "auth"
+export const { GET, POST } = handlers
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,16 +6,22 @@ export default function Navigation() {
     const {data: session} = useSession()
     if (session) {
         return (
-            <>
-                Signed in as {session.user.email} <br/>
-                <button onClick={() => signOut()}>Sign out</button>
-            </>
+            <div className="flex flex-col justify-center items-center">
+                Signed in as {session.user.email}
+                <button className='min-w-40 border border-green-500 mt-8 hover:bg-[#D50000] px-4 py-1.5 rounded'
+                        onClick={() => signOut()}>Sign out
+                </button>
+            </div>
         )
     }
     return (
-        <>
-            Not signed in <br/>
-            <button onClick={() => signIn()}>Sign in</button>
-        </>
+        <div className="flex flex-col justify-center items-center">
+            <div className='flex border border-[#D50000] px-2 py-1.5 mt-2 rounded hover:text-[#D50000]'>{
+                !!session ? `Юзер ${session?.user?.email} залогинен` : 'Нет в сессии никакого юзера!'}
+            </div>
+                <button className='min-w-40 border border-green-500 mt-4 hover:bg-[#50d71e]  py-1.5 rounded'
+                    onClick={() => signIn()}>Sign in
+            </button>
+        </div>
     )
 }
