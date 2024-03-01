a4210e4358cc64b3c1290f24c42213e4c0e73135
dmrado <dmrado@live.ru>
2024-03-01 10:56:27 +0300

    Update connection.ts

--- src/app/db/connection.ts
+++ src/app/db/connection.ts
@@ -1,13 +1,14 @@
-import { config } from 'dotenv';
-import { Sequelize } from "sequelize-typescript"
+import { config } from 'dotenv'
+import { Sequelize } from 'sequelize-typescript'
 import mysql2 from 'mysql2'
+
 config()
 
 export const sequelize = new Sequelize({
-        port: Number(process.env.DB_PORT) || 8889,
-        username: process.env.DB_LOGIN,
-        password: process.env.DB_PASSWORD,
-        dialect: 'mysql',
-        dialectModule: mysql2,
-        database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT) || 8889,
+    username: process.env.DB_LOGIN,
+    password: process.env.DB_PASSWORD,
+    dialect: 'mysql',
+    dialectModule: mysql2,
+    database: process.env.DB_NAME,
 }) as Sequelize
