430c28d2a45e90fb41d067fbff95df5db3f97947
Vladimir Kromm <vladimir.kromm@mercedes-benz.com>
2024-06-16 17:10:55 +0200

    basic loading

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,26 +1,22 @@
 'use client'
-import {useEffect, useState} from 'react'
-import React, {Suspense} from 'react'
-import {useInView} from 'react-intersection-observer'
-import {Post, PostPreview} from '@/app/db/post.model'
+import { useEffect, useState } from 'react'
+import React from 'react'
+import { useInView } from 'react-intersection-observer'
+import { PostPreview } from '@/app/db/post.model'
 import PostsPreview from '@/components/PostsPreview.tsx'
-import {getPosts} from '@/app/actions/getPosts.ts'
-import {NUMBER_OF_POSTS_TO_FETCH} from '@/app/constants.ts'
-import PostsLoading from "@/components/PostLoading.tsx";
+import { getPosts } from '@/app/actions/getPosts.ts'
+import { NUMBER_OF_POSTS_TO_FETCH } from '@/app/constants.ts'
 
-export default function PostList({initialPosts}: { initialPosts: PostPreview[] }) {
-    const [offset, setOffset] = useState(NUMBER_OF_POSTS_TO_FETCH)
-    const [count, setCount] = useState(0)
-    // const [ loading, setLoading ] = useState(false)
+export default function PostList({ initialPosts }: { initialPosts: PostPreview[] }) {
+    const [ offset, setOffset ] = useState(NUMBER_OF_POSTS_TO_FETCH)
+    const [ count, setCount ] = useState(0)
 
-    const [posts, setPosts] = useState<PostPreview[]>(initialPosts)
-    const {ref, inView} = useInView()
-    // new Promise(resolve => setTimeout(resolve, 5000))
-
+    const [ posts, setPosts ] = useState<PostPreview[]>(initialPosts)
+    const { ref, inView } = useInView()
     const loadMorePosts = async () => {
         //присваиваем значение posts в переменную newPosts деструктуризация с переимнованием
-        const {posts: newPosts, count} = await getPosts(offset, NUMBER_OF_POSTS_TO_FETCH)
-        setPosts([...posts, ...newPosts])
+        const { posts: newPosts, count } = await getPosts(offset, NUMBER_OF_POSTS_TO_FETCH)
+        setPosts([ ...posts, ...newPosts ])
         setOffset(offset + NUMBER_OF_POSTS_TO_FETCH)
         setCount(count)
     }
@@ -29,17 +25,13 @@ export default function PostList({initialPosts}: { initialPosts: PostPreview[] }
         if (inView && posts.length !== count) {
             loadMorePosts()
         }
-    }, [inView])
+    }, [ inView ])
 
     return (
         <div>
-
-            {posts.map((post) =>
-                // <Suspense fallback={<PostsLoading/>}>
-                    <PostsPreview key={post.id} post={post}/>
-                //*</Suspense>*/}
+            {posts.map((post, idx) =>
+                <PostsPreview key={post?.id || idx} post={post}/>
             )}
-
             <div className='flex justify-center my-10 text-my_l_green' ref={ref}>
                 {posts.length !== count ? 'Загружаем еще...' : 'Простите, это весь список, больше пока не написали!'}
             </div>
--- src/components/PostsPreview.tsx
+++ src/components/PostsPreview.tsx
@@ -1,33 +1,34 @@
 import React from 'react'
 import Link from 'next/link'
-import {PostPreview} from '@/app/db/post.model'
+import { PostPreview } from '@/app/db/post.model'
 
-const PostsPreview = ({post}: { post: PostPreview }) => {
-    // console.log('post on PostsPreview', post)
+const PostsPreview = ({ post }: { post: PostPreview|undefined }) => {
+    console.log('PostPreview', post)
     return <li>
         <div className="flex flex-col mt-20 h-full px-0 py-30 content-center items-center">{/*card*/}
 
             <div className="overflow-hidden h-full rounded-lg">{/*card wrapper для эффекта hover-scale картинки*/}
                 <img className="transform hover:scale-150 ease-in-out duration-700 w-full h-fit rounded-lg"
-                     src={post.path ? post.path : 'img/postspage/cloudsWIDE.webp'} alt="Post image"/>{/*card-img*/}
+                    src={post && post.path ? post.path : 'img/postspage/cloudsWIDE.webp'} alt="Post image"/>{/*card-img*/}
             </div>
 
             <div
                 className="flex flex-col justify-between h-52 sm:h-60 w-96 md:w-[24rem] min-h-40 -mt-4 sm:-mt-24 z-10 p-3 overflow-hidden bg-indigo-50  ease-in-out duration-300 border-t-8 border-transparent hover:border-t-8 hover:border-[#004E98]">{/*card-body*/}
                 <h5 className="text-center text-blue-950 font-semibold mt-4">
-                    <Link
+                    {post ? <Link
                         className="w-12 p-2 !text-[#004E98] hover:!text-orange-500 transform hover:scale-150 ease-in-out duration-300 text-2xl"
-                        href={`/posts/${post.id}`}>{post.title?.slice(0, 45)}...</Link>
+                        href={`/posts/${post.id}`}>{post.title?.slice(0, 45)}...</Link> : <p>...Loading...</p>
+                    }
                 </h5>
-                <p className="text-justify text-blue-950 px-2">&nbsp;{post.preview}...</p>
+                <p className="text-justify text-blue-950 px-2">&nbsp;{post ? post.preview : 'Text is loading...'}...</p>
 
                 <p className="text-end text-blue-950 italic pr-2">Опубликовано:&nbsp;
-                    {post.createdAt?.toLocaleDateString('ru-RU', {
+                    {post ? post.createdAt?.toLocaleDateString('ru-RU', {
                         // weekday: 'short',
                         year: 'numeric',
                         month: 'long',
                         day: 'numeric'
-                    })}
+                    }) : 'YY-DD-MM'}
                 </p>
             </div>
         </div>
