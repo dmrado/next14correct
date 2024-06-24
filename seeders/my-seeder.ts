26def05bef5a8c024324a6364d944966d0347c40
Vladimir Kromm <vladimir.kromm@mercedes-benz.com>
2024-06-24 10:38:31 +0200

    one more fix

--- seeders/my-seeder.ts
+++ seeders/my-seeder.ts
@@ -1,20 +1,23 @@
 import { Post } from '@/app/db/post.model'
 import { faker } from '@faker-js/faker'
 
-type PostData = Pick<Post, 'title'| 'text'>
+type PostData = Pick<Post, 'title'| 'text'| 'preview'>
 
 const fakePostDataFactory = (number = 10, suffix = ''): Array<PostData> => {
-    return [ ...Array(number).keys() ].map(() => ({
-        title: faker.lorem.sentence() + suffix,
-        text: faker.lorem.text().substring(0, 200)
-    }))
+    return Array(number).map(() => {
+        const text = faker.lorem.text().substring(0, 200)
+        return {
+            title: faker.lorem.sentence() + suffix,
+            text: text,
+            preview: text
+        }})
 }
 
 const seedPost = async () => {
-    await Post.create({ title: 'New Post', text: 'Post from seeder' })
+    await Post.create({ title: 'New Post', text: 'Post from seeder', preview: 'Post from seeder' })
     const postData = fakePostDataFactory(3)
     await Promise.all(postData.map(async (post) => {
-        return await Post.create({ title: post.title, text: post.text })
+        return await Post.create({ title: post.title, text: post.text, preview: post.preview })
     }))
     const postData2 = fakePostDataFactory(3, '_bulk')
     await Post.bulkCreate(postData2)
