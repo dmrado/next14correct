18159fa5c1233cddce9e9fa0230e63fc49b911ff
dmrado <8838525@gmail.com>
2025-11-27 19:09:06 +0300

    Update CookieConsent.tsx

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -1,11 +1,11 @@
 'use client'
-import { useState } from 'react'
-import { setConsentAccepted } from '@/app/actions/setCookiesAccepted.ts'
+import {useState} from 'react'
+import {setConsentAccepted} from '@/app/actions/setCookiesAccepted.ts'
 import Link from 'next/link'
 
-const CookieConsent = ({ isAccepted }: {isAccepted: boolean}) => {
+const CookieConsent = ({isAccepted}: { isAccepted: boolean }) => {
 
-    const [ showMessage, setShowMessage ] = useState(true)
+    const [showMessage, setShowMessage] = useState(true)
     const handleAccept = async () => {
         await setConsentAccepted()
         setShowMessage(false)
@@ -15,27 +15,40 @@ const CookieConsent = ({ isAccepted }: {isAccepted: boolean}) => {
 
     return (
         <div
-            className="flex flex-col fixed bottom-0 bg-gray-600 rounded pt-2 px-2 text-white text-s text-center z-20">
-            На этом сайте мы используем файлы cookie, чтобы улучшить ваш опыт использования и предоставить
-            персонализированный контент. Некоторые из этих файлов cookie устанавливаются сторонними сервисами, такими
-            как Google (Google является зарегистрированным товарным знаком Google LLC) и Яндекс (Яндекс является зарегистрированным товарным знаком Яндекса).
+            className="fixed bottom-0 left-1/2 transform -translate-x-1/2 z-50 shadow-2xl
+        w-11/12      // На мобильных: Занимаем 91.6% ширины экрана
+        max-w-xl     // Максимальная ширина на всех экранах (xl = 36rem/576px)
+        md:max-w-lg  // На средних экранах max 32rem/512px
 
-            Мы используем Google reCAPTCHA для защиты нашего сайта от злоумышленников и
-            спама. Google reCAPTCHA помогает нам убедиться, что вы человек, а не автоматизированная программа.
+        // Стиль:
+        flex flex-col
+        bg-gray-700 rounded-lg p-4 text-white text-sm text-left space-y-3">
+            <div
+                className="flex flex-col fixed bottom-0 text-center z-20">
+                На этом сайте мы используем файлы cookie, чтобы улучшить ваш опыт использования и предоставить
+                персонализированный контент. Некоторые из этих файлов cookie устанавливаются сторонними сервисами,
+                такими
+                как Google (Google является зарегистрированным товарным знаком Google LLC) и Яндекс (Яндекс является
+                зарегистрированным товарным знаком Яндекса).
 
-            Мы также используем Яндекс.Карты для предоставления вам удобных карт и
-            местоположений на нашем сайте. Это поможет вам быстро найти нужные адреса и маршруты.
+                Мы используем Google reCAPTCHA для защиты нашего сайта от злоумышленников и
+                спама. Google reCAPTCHA помогает нам убедиться, что вы человек, а не автоматизированная программа.
 
-            Кроме того, как разработчик этого сайта, мы используем файлы cookie для сохранения вашего выбора
-            относительно использования файлов cookie. Для получения более подробной информации о том, как мы обрабатываем ваши данные пожалуйста, обратитесь к нашей
-            <Link href={'/policy'}><u>Политике конфиденциальности в отношении персональных данных</u></Link>
-            <br/>
-            Продолжая использовать наш сайт, вы соглашаетесь с использованием указанных файлов cookie в
-            соответствии с указанной Политикой конфиденциальности в отношении персональных данных.
+                Мы также используем Яндекс.Карты для предоставления вам удобных карт и
+                местоположений на нашем сайте. Это поможет вам быстро найти нужные адреса и маршруты.
 
-            <form action={handleAccept}>
-                <button className='btn btn-blog' style={{ marginRight: '21px' }}>Согласен</button>
-            </form>
+                Кроме того, как разработчик этого сайта, мы используем файлы cookie для сохранения вашего выбора
+                относительно использования файлов cookie. Для получения более подробной информации о том, как мы
+                обрабатываем ваши данные пожалуйста, обратитесь к нашей
+                <Link href={'/policy'}><u>Политике конфиденциальности в отношении персональных данных</u></Link>
+                <br/>
+                Продолжая использовать наш сайт, вы соглашаетесь с использованием указанных файлов cookie в
+                соответствии с указанной Политикой конфиденциальности в отношении персональных данных.
+
+                <form action={handleAccept}>
+                    <button className='btn btn-blog' style={{marginRight: '21px'}}>Согласен</button>
+                </form>
+            </div>
         </div>
     )
 }
